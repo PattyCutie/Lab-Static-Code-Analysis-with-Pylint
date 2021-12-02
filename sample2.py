9252edb5358c1b3e80a53aa914f37e11dd8213dc
PattyCutie <iamsureiamcute@gmail.com>
2021-12-02 16:15:41 +0000

    Lab Static Code Analysis Lab

---
+++ sample2.py
@@ -0,0 +1,9 @@
+""" This is add function"""
+def add(number1, number2):
+    """ This is the function return sum of given numbers """
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
